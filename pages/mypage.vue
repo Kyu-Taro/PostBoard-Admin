44ef4d3730ed64bf656b08b21581e3e686afac4d
Kyutaro <gtnine999@gmail.com>
2020-02-19 16:25:58 +0900

    マイページ投稿表示機能追加

--- pages/mypage.vue
+++ pages/mypage.vue
@@ -15,6 +15,34 @@
         <v-btn class="orange" text href="/delete">delete</v-btn>
       </v-card-actions>
     </v-card>
+
+    <div v-for="data in datas" :key="data.id">
+      <v-card class="mx-auto" dark max-width="400">
+        <v-card-title>
+          <v-icon large left>
+            mdi-twitter
+          </v-icon>
+          <span class="title font-weight-light">BOYAKI</span>
+        </v-card-title>
+
+        <v-card-text class="headline font-weight-bold">
+          {{ data.text }}
+        </v-card-text>
+
+        <v-card-actions>
+          <v-list-item class="grow">
+            <v-list-item-avatar color="grey darken-3">
+              <v-img class="elevation-6" src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"></v-img>
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ data.id }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card-actions>
+      </v-card>
+    </div>
+
   </div>
 </template>
 
@@ -55,5 +83,9 @@
   }
   .orange .v-btn__content {
     color: orange;
+  }
+  .v-card {
+    margin: 50px auto;
+    background: black;
   }
 </style>
